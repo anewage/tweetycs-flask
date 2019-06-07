1c4382d648d9f2df7a4407a960dd447168bfcc5d
verinachen <verinachen@outlook.com>
2019-06-06 22:47:17 -0400

    Update MLapp.py

--- flask-mldl/mlapp.py
+++ flask-mldl/mlapp.py
@@ -1,108 +1,131 @@
 from flask import Flask
-from flask import request
+from flask import request,jsonify
 import pandas as pd 
 import numpy as np 
 import pickle
 from helperClasses import *
+import json
+from pandas.io.json import json_normalize
 
 app = Flask(__name__)
 
-@app.route('/svmuser', methods=['GET','POST'])
-def SVMuser():
-	model = open("SVMUserOnly.pickle","rb")
-	clf = pickle.load(model)
-	#modify it for the data stream
-	df= pd.read_csv("test1.csv").fillna('')
-	parameterIn=['user_description']
-	X_test = df[parameterIn]
-	bTest = clf.predict(X_test)
-	result=str(bTest)
-	return result
 
 @app.route('/svmtweet', methods=['GET','POST'])
 def SVMtweet():
 	model = open("SVMTweetOnly.pickle","rb")
 	clf = pickle.load(model)
-	#modify it for the data stream
-	df= pd.read_csv("test1.csv").fillna('')
+	data=request.get_json(force=True)
+	df = pd.DataFrame.from_dict(json_normalize(data))
+	#data=pd.read_json(data)
+	#json_normalize(data['text'])
 	parameterIn=['tweet']
 	X_test = df[parameterIn]
 	bTest = clf.predict(X_test)
-	result=str(bTest)
-	return result
+	result=bTest
+	result=result[0]
+	return jsonify(theme=result)
+
+@app.route('/svmuser', methods=['GET','POST'])
+def SVMuser():
+	model = open("SVMUserOnly.pickle","rb")
+	clf = pickle.load(model)
+	data=request.get_json(force=True)
+	df = pd.DataFrame.from_dict(json_normalize(data))
+	parameterIn=['user_description']
+	test = df[parameterIn]
+	bTest=clf.predict(test)
+	result=bTest
+	result=result[0]
+	return jsonify(group=result)
 
 @app.route('/knnuser', methods=['GET','POST'])
 def KNNuser():
 	model = open("KNNUserOnly.pickle","rb")
 	clf = pickle.load(model)
 	#modify it for the data stream
-	df= pd.read_csv("test1.csv").fillna('')
+	data=request.get_json(force=True)
+	df = pd.DataFrame.from_dict(json_normalize(data))
 	parameterIn=['user_description']
-	X_test = df[parameterIn]
-	bTest = clf.predict(X_test)
-	result=str(bTest)
-	return result
+	test = df[parameterIn]
+	bTest=clf.predict(test)
+	result=bTest
+	result=result[0]
+	return jsonify(group=result)
 
 @app.route('/knntweet', methods=['GET','POST'])
 def KNNtweet():
 	model = open("KNNTweetOnly.pickle","rb")
 	clf = pickle.load(model)
-	#modify it for the data stream
-	df= pd.read_csv("test1.csv").fillna('')
+	data=request.get_json(force=True)
+	df = pd.DataFrame.from_dict(json_normalize(data))
+	#data=pd.read_json(data)
+	#json_normalize(data['text'])
 	parameterIn=['tweet']
 	X_test = df[parameterIn]
 	bTest = clf.predict(X_test)
-	result=str(bTest)
-	return result
+	result=bTest
+	result=result[0]
+	return jsonify(theme=result)
 
 @app.route('/rfuser', methods=['GET','POST'])
 def RFuser():
 	model = open("RFUserOnly.pickle","rb")
 	clf = pickle.load(model)
 	#modify it for the data stream
-	df= pd.read_csv("test1.csv").fillna('')
+	data=request.get_json(force=True)
+	df = pd.DataFrame.from_dict(json_normalize(data))
 	parameterIn=['user_description']
-	X_test = df[parameterIn]
-	bTest = clf.predict(X_test)
-	result=str(bTest)
-	return result
+	test = df[parameterIn]
+	bTest=clf.predict(test)
+	result=bTest
+	result=result[0]
+	return jsonify(group=result)
 
 @app.route('/rftweet', methods=['GET','POST'])
 def RFtweet():
 	model = open("RFTweetOnly.pickle","rb")
 	clf = pickle.load(model)
-	#modify it for the data stream
-	df= pd.read_csv("test1.csv").fillna('')
+	data=request.get_json(force=True)
+	df = pd.DataFrame.from_dict(json_normalize(data))
+	#data=pd.read_json(data)
+	#json_normalize(data['text'])
 	parameterIn=['tweet']
 	X_test = df[parameterIn]
 	bTest = clf.predict(X_test)
-	result=str(bTest)
-	return result
+	result=bTest
+	result=result[0]
+	return jsonify(theme=result)
 
 @app.route('/mlpuser', methods=['GET','POST'])
 def MLPuser():
 	model = open("MLPUserOnly.pickle","rb")
 	clf = pickle.load(model)
 	#modify it for the data stream
-	df= pd.read_csv("test1.csv").fillna('')
+	data=request.get_json(force=True)
+	df = pd.DataFrame.from_dict(json_normalize(data))
 	parameterIn=['user_description']
-	X_test = df[parameterIn]
-	bTest = clf.predict(X_test)
-	result=str(bTest)
-	return result
+	test = df[parameterIn]
+	bTest=clf.predict(test)
+	result=bTest
+	result=result[0]
+	return jsonify(group=result)
+
 
 @app.route('/mlptweet', methods=['GET','POST'])
 def MLPtweet():
 	model = open("MLPTweetOnly.pickle","rb")
 	clf = pickle.load(model)
-	#modify it for the data stream
-	df= pd.read_csv("test1.csv").fillna('')
+	data=request.get_json(force=True)
+	df = pd.DataFrame.from_dict(json_normalize(data))
+	#data=pd.read_json(data)
+	#json_normalize(data['text'])
 	parameterIn=['tweet']
 	X_test = df[parameterIn]
 	bTest = clf.predict(X_test)
-	result=str(bTest)
-	return result
+	result=bTest
+	result=result[0]
+	return jsonify(theme=result)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=33507)
+    app.run(debug=True, port=33508)
